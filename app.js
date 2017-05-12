d6a46a701c297fdba14fdda5648d6ade92621479
shankar <tinker-cool@mail.ru>
2017-05-12 12:10:13 +0700

    2

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log(window.location);
+var Network = function() {
+	this.io 	= undefined;
+	this.url 	= "";
+	this.port	= 8008;
+}
+
+Network.prototype.getUrl = function() {
+	var location = window.location;
+	var protocol = location.protocol == "http:" ? "http://" : "https://";
+	this.url     = protocol+location.host;
+	console.log(this.url);
+}
+
+Network.getUrl();
